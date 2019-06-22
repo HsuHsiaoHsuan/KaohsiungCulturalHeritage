30efb7c9640e78b3d98728edb08c2396ba4533eb
Freeman <freeman@mbp>
2019-06-22 19:40:13 +0800

    [gradle] turning project build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:3.5.0-beta04")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("org.junit.platform:junit-platform-gradle-plugin:1.2.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -23,6 +24,6 @@ allprojects {
     }
 }
 
-tasks.register("clean", Delete::class) {
+tasks.register("clean", type = Delete::class) {
     delete(rootProject.buildDir)
 }
